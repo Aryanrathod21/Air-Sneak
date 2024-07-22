d61a16c23df9e9600be8a7b8447b0982c62625ef
Aryanrathod21 <aryanrathod410@gmail.com>
2024-07-22 19:37:57 +0530

    typescript error

--- my-web-store-storefront/src/modules/home/components/trending/index.tsx
+++ my-web-store-storefront/src/modules/home/components/trending/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 
 type Props = {
-    className: string;
+    // className: string;
 };
 
 const Trending = (props: Props) => {
