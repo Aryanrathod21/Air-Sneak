26599adba7bb874fb4f562fe03d440f7f3ad6d7b
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-25 14:55:36 +0530

    review children

--- my-web-store-storefront/src/modules/home/components/swiper/index.tsx
+++ my-web-store-storefront/src/modules/home/components/swiper/index.tsx
@@ -1,15 +1,15 @@
-// components/Slider.js
+"use client"
 
-'use client'; // Required for client-side rendering in Next.js 13+ with the app directory structure
+import women from "../../../../../public/woman.png";
 
-import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/scrollbar';
-import { Scrollbar } from 'swiper/modules';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import { Scrollbar } from "swiper/modules";
 
-import './swiper.css';
-import Testimonial from '../testimonial';
+import "./swiper.css";
+import Testimonial from "../testimonial";
 
 export default function Slider() {
   return (
@@ -18,10 +18,30 @@ export default function Slider() {
       modules={[Scrollbar]}
       className="mySwiper"
     >
-      <SwiperSlide><Testimonial/></SwiperSlide>
-      <SwiperSlide><Testimonial/></SwiperSlide>
-      <SwiperSlide><Testimonial/></SwiperSlide>
-      <SwiperSlide><Testimonial/></SwiperSlide>
+      <SwiperSlide>
+        <Testimonial imageSrc={women.src}>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        </Testimonial>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Testimonial imageSrc="https://via.placeholder.com/100">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        </Testimonial>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Testimonial imageSrc="https://via.placeholder.com/100">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        </Testimonial>
+      </SwiperSlide>
+      <SwiperSlide>
+        <Testimonial imageSrc="https://via.placeholder.com/100">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        </Testimonial>
+      </SwiperSlide>
     </Swiper>
   );
 }
--- my-web-store-storefront/src/modules/home/components/testimonial/index.tsx
+++ my-web-store-storefront/src/modules/home/components/testimonial/index.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { ReactNode } from "react";
 
-type Props = {};
+type Props = {
+  imageSrc: string;
+  children?: ReactNode;
+};
 
-const Testimonial = (props: Props) => {
+const Testimonial = ({ imageSrc, children }: Props) => {
   return (
     <div className="flex justify-center items-center xl:px-12 lg:px-6 md:px-4 sm:px-0 py-12">
       <div className="relative bg-white shadow-lg rounded-lg p-8 max-w-2xl w-full text-center">
-        {/* Circle Image */}
         <div className="absolute left-1/2 transform -translate-x-1/2 -top-12">
           <img
-            src="https://via.placeholder.com/100"
+            src={imageSrc}
             alt="User"
             className="w-24 h-24 rounded-full border-4 border-white object-cover"
+            style={{ width: "100px", height: "100px" }} // Added inline styles to enforce 100x100 size
           />
         </div>
-        
-        {/* Text Content */}
+
         <div className="mt-16">
           <h3 className="text-xl font-semibold">John Doe</h3>
           <p className="mt-4 text-gray-600">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            {children
+              ? children
+              : `"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."`}
           </p>
         </div>
       </div>
