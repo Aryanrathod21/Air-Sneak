6685d7eb656e49c2402a13fa71e17d7465dcb0d8
Aryan Rathod <115866974+Aryanrathod21@users.noreply.github.com>
2024-08-27 10:33:44 +0530

    query/initial ui

--- my-web-store-storefront/src/app/[countryCode]/(main)/query/page.tsx
+++ my-web-store-storefront/src/app/[countryCode]/(main)/query/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 const Query = (props: Props) => {
   return (
-    <div>Query</div>
-  )
-}
+    <div className="flex flex-col h-screen justify-between max-w-md mx-auto p-4 bg-white shadow-lg rounded-lg">
+      <div className="overflow-y-auto flex-1 p-4 bg-gray-100 rounded-lg">
+        <div className="text-sm text-gray-600">Hello! How can I help you today?</div>
+      </div>
+      <div className="flex items-center p-2 mt-4 bg-white rounded-lg border-t">
+        <input
+          type="text"
+          placeholder="Type a message..."
+          className="flex-1 px-4 py-2 text-sm border rounded-lg focus:outline-none"
+        />
+        <button className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+          Send
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Query
+export default Query;
